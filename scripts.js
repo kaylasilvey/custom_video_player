fe60ff2f21113a2ae0dd94f71054893c681000b3
Michael <michaeleburrell@gmail.com>
2020-07-30 17:19:17 -0700

    updates to cloudinary and voice controls

--- scripts.js
+++ scripts.js
@@ -17,12 +17,12 @@ const list = document.getElementById("videos");
 
 const uploadWidget = cloudinary.createUploadWidget(
   {
-    cloudName: "heydanhey",
+    cloudName: "eburrell",
     tags: ["video"],
     resourceType: "video",
     multiple: false,
     clientAllowedFormats: ["mp4", "mov"],
-    uploadPreset: "tycwqdaj"
+    uploadPreset: "fwmyv5i7"
   },
   function(error, result) {
     if (!error && result && result.event === "success") {
@@ -75,18 +75,18 @@ function toggleFullscreen() {
   video.requestFullscreen();
 }
 function getVideos() {
-  fetch("https://res.cloudinary.com/heydanhey/video/list/video.json")
-    .then(response => response.json())
-    .then(data => addVideos(data.resources));
+  fetch("https://res.cloudinary.com/eburrell/video/list/video.json")
+    .then((response) => response.json())
+    .then((data) => addVideos(data.resources));
 }
 
 function addVideos(videos) {
   console.log(videos);
-  videos.forEach(video => {
+  videos.forEach((video) => {
     let li = document.createElement("li");
     li.innerHTML = video.created_at;
     const att = document.createAttribute("id");
-    att.value = `https://res.cloudinary.com/heydanhey/video/upload/v${video.version}/${video.public_id}.${video.format}`;
+    att.value = `https://res.cloudinary.com/eburrell/video/upload/v${video.version}/${video.public_id}.${video.format}`;
     li.setAttributeNode(att);
     li.addEventListener("click", loadVideo);
     list.appendChild(li);
@@ -149,14 +149,14 @@ fullscreen.addEventListener("click", toggleFullscreen);
 
 let mousedown = false;
 progress.addEventListener("click", scrub);
-progress.addEventListener("mousemove", e => mousedown && scrub(e));
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
 
 toggle.addEventListener("click", togglePlay);
-skipButtons.forEach(button => button.addEventListener("click", skip));
-ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
-ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) => range.addEventListener("mousemove", handleRangeUpdate));
 
 video.addEventListener("canplay", paintToCanvas);
 
@@ -175,6 +175,7 @@ recognition.interimResults = true;
 recognition.lang = "en-US";
 
 function checkForControls(transcript) {
+  console.log(transcript);
   const PLAY_COMMAND = "play video";
   if (transcript.includes(PLAY_COMMAND)) {
     togglePlay();
@@ -189,10 +190,11 @@ function checkForControls(transcript) {
   }
 }
 
-recognition.addEventListener("result", e => {
+recognition.addEventListener("result", (e) => {
+  console.log(e);
   const transcript = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
     .join("");
 
   if (e.results[0].isFinal) {
@@ -202,4 +204,5 @@ recognition.addEventListener("result", e => {
 });
 
 recognition.addEventListener("end", recognition.start);
+console.log(recognition);
 recognition.start();
